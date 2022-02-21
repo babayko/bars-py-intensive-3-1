5283bc0fd606c682cd870973e6c0717c711959bf
Maksim Kuzmin <lacoast@mail.ru>
2022-02-21 21:47:28 +0500

    Первый вариант: решены задачи по темам flow_control и scope

--- scope/task_2/question.py
+++ scope/task_2/question.py
@@ -4,11 +4,23 @@
 
 
 def transmit_to_space(message):
-   
-    def data_transmitter():        
+
+    def data_transmitter():
         print(message)
 
     data_transmitter()
 
 
-print(transmit_to_space("Test message"))
+print(transmit_to_space('Test message'))
+
+"""
+скрипт выведет:
+
+Test message
+None
+
+Test message - результат выполнения функции data_transmitter() - печать;
+None  - результат выполнения функции transmit_to_space(), так как она
+ничего не возвращает
+
+"""
--- scope/task_4/implementation.py
+++ scope/task_4/implementation.py
@@ -5,10 +5,14 @@ def bad_open(file_path, mode):
 
 def open_and_close_file(file_path):
     """Открывет и закрывает файл
-
     Args:
         file_path: путь до файла
     """
     open = bad_open
+    
+    del open
+    import builtins
+    open = builtins.open
+    
     f = open(file_path, 'r')
     f.close()
